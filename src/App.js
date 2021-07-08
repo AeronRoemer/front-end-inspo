a83e0d125b7a6668d2c3d0a09e1e47b25be43a4e
x <57688724+AeronRoemer@users.noreply.github.com>
2021-07-08 11:50:24 -0600

    Rendering Boards and Passing ID

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
 import { useEffect, useState } from 'react'; 
 import axios from 'axios';
+import Board from './components/Board.js'
 
 import './App.css';
 
 function App() {
   // App holds data for current board and a list of all boards
-  const [board, setBoard] = useState(null)
+  const [boardId, setBoard] = useState(null)
   const [boards, setBoards] = useState([])
   // load boards once on render
   useEffect(()=>{
-    axios.get('http://localhost:5000/boards').then((res)=>{
-      console.log(res.data)
+    axios.get('http://localhost:5000/boards')
+    .then((res)=>{
       setBoards(res.data)
-      console.log(boards)
     })
   }, [])
+  // click handler given to each board title in the list
+  const clickSetBoard = (id) =>{
+    setBoard(id)
+  }
+  const boardsList = boards.map((board, index)=> {
+    console.log(board)
+    return <li key={index} onClick={() => clickSetBoard(board.board_id)}>{board.title}</li>
+  })
+  console.log('setting board', boardId)
   return (
     <div className="App">
       <div className="baords-display-container">
         <h2>Available Boards</h2>
         <ul>
-        {boards.forEach(board=> {
-          <li key={board.id}>{board}</li>
-        })}
+        {boardsList}
         </ul>
+        <Board id={boardId} />
       </div>
     </div>
   );
---
+++ src/components/Board.js
@@ -0,0 +1,9 @@
+import { useState, useEffect } from "react";
+
+const Board = (props) =>{
+
+    return(
+        <h2>Hi, I'm {props.id}</h2>
+    )
+}
+export default Board;
